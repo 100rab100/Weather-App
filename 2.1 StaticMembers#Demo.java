342eb71f836ca6297cd38b8097835234f690a9d4
100rab100 <99138605+100rab100@users.noreply.github.com>
2022-11-05 08:45:28 +0530

    Update 2.1 StaticMembers#Demo.java

---
+++ 2.1 StaticMembers#Demo.java
@@ -0,0 +1,19 @@
+class HondaCity 
+{ 
+ static long price=10; 
+ int a,b; 
+ static double OnRoadPrice(String city) 
+ { 
+ Switch(city) 
+ { 
+ Case “Delhi”: 
+ return price+price*0.1; 
+ case “Mumbai”: 
+ return price+preice*0.09; 
+ } 
+ } 
+} 
+class test 
+{ 
+ public static void main() 
+ { }
